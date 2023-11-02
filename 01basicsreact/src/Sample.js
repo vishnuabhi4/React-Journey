4a909efc1651b9451f497b76822a9fe7cc3027b2
Vishnu V <vishnuabhi4@gmail.com>
2023-11-02 21:01:31 +0530

    .

---
+++ 01basicsreact/src/Sample.js
@@ -0,0 +1,10 @@
+export default Sample;
+
+function Sample(){
+    return(
+        <>
+        <h1>Sample</h1>
+        <p>sample test for react</p>
+        </>
+    );
+};
--- 01basicsreact/src/index.js
+++ 01basicsreact/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //ReactDOM is used for rendering React components into the DOM.
 import App from './App';
+import Sample from './Sample';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<>
+<App />
+<Sample />
+</>
 );
 
