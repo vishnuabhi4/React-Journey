dcbf8c213692b6423f6e214b7dbc4fd8e90e3c4d
Vishnu V <vishnuabhi4@gmail.com>
2023-12-04 22:58:08 +0530

    color changer

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -8,9 +8,30 @@ function App() {
 
   return (
     <>
-  <div className="min-h-screen w-full duration-200"
-  style={{backgroundColor:color}}
-  ></div>
+      <div
+        className="min-h-screen w-full duration-200 flex-auto"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-20 inset-x-0 px-2">
+          <div className="fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+            <button
+            onClick={()=>{setcolor("red")}}
+              className="outline-none px-4 py-1 rounded-full"
+              style={{background:"red"}}
+            >Red</button>
+            <button
+            onClick={()=>{setcolor("blue")}}
+              className="outline-none px-4 py-1 rounded-full"
+              style={{background:"blue"}}
+            >Blue</button>
+            <button
+            onClick={()=>{setcolor("green")}}
+              className="outline-none px-4 py-1 rounded-full"
+              style={{background:"green"}}
+            >Green</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
