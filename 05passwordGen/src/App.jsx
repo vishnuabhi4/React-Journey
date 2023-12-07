bb012dc47521d34a835c4457a3c1a42fd8756dbd
Vishnu V <vishnuabhi4@gmail.com>
2023-12-07 22:15:09 +0530

    .

--- 05passwordGen/src/App.jsx
+++ 05passwordGen/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+const [length, setLength] = useState(8)
+const [numberAllowed, setNumberAllowed] = useState(false)
+const [characterAllowed, setCharactersAllowed] = useState(false)
+cosst [password, setPassword] = useState(" ")
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +12,14 @@ function App() {
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md 
-      rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>test</div>
+      rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3 '
+        />
+        </div>
+      </div>
     </>
   )
 }
